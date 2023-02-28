a52301ff8dfa794cadf14a4fd166e9dcaccc4042
Jaodun Muntasir <126531434+jaodunmuntasir@users.noreply.github.com>
2023-02-28 02:28:41 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Program5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Number of kids: ");
+            int kids = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Number of apples: ");
+            int apples = Convert.ToInt32(Console.ReadLine());
+
+            int divisibleapples = apples - (apples % kids);
+            int perkid = divisibleapples / kids;
+            int remainingapples = apples % kids;
+
+            Console.WriteLine("Every kid gets {0} apples and there are {1} apples left.", perkid, remainingapples);
+        }
+    }
+}
